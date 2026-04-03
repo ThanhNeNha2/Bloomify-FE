d545a79615cb6d8fb65c4b197e0c76adf0560e74
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2026-04-03 10:46:10 +0000

    Refactor login page to use Vue 3 composition API, integrate HomePageContext, and enhance styling with scoped CSS

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,13 +1,49 @@
-<template>
-  <div>
+<script>
+import {
+  defineComponent,
+  ref
+} from 'vue';
+import HomePageContext from './HomePageContext';
 
+ 
+export default defineComponent({
+  name: 'HomeView',
+
+  setup () {
+    const displayNameRef = ref('Vo Chi Thanh');
+
+    const context = HomePageContext.create({
+      displayNameRef
+    })
+      .setupComponent();
+
+    return {
+      context
+    };
+  }
+});
+</script>
+
+<template>
+  <div class="unit-home">
+    <p class="greeting">
+      {{ context.formatGreetingLine() }}
+    </p>
+
+ 
   </div>
 </template>
 
-<script setup>
+<style scoped>
+.unit-home {
+  background-color: var(--background-color);
+}
 
-</script>
+.unit-home > .greeting {
+  margin-block-end: 0.5rem;
+}
 
-<style lang="scss" scoped>
-
+.unit-home > .name {
+  color: var(--color-brown);
+}
 </style>
