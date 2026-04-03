227ab34ac8cabace3918c118c0a3de7a9c905430
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2026-04-03 08:21:53 +0000

    Refactor index, login, and register pages to remove unused code and simplify structure

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,47 +1,13 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
 <script setup>
 
 </script>
 
-<template>
-  <div class="unit-login">
-    <div class="right">
-      anh
-    </div>
-    <div class="left">
-      <h1>Login</h1>
-      <span>
-        Chào mừng bạn đến với ứng dụng lưu trữ nhật ký cá nhân
-      </span>
-      <form>
-        <label for="email">Email</label>
-        <input type="text" placeholder="Email" id="email" />
-        <label for="password">Password</label>
-        <input type="password" placeholder="Password" id="password" />
-        <button type="submit">Login</button>
-      </form>
-      <span>New User? <a href="/register">Register</a></span>
-    </div>
-  </div>
-</template>
+<style lang="scss" scoped>
 
-<style scoped>
-.unit-login {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 100vh;
-  width: 100%;
-}
-
-.unit-login>.right {
-  width: 50%;
-  height: 100%;
-  background-color: #c2c2c2;
-}
-
-.unit-login>.left {
-  width: 50%;
-  height: 100%;
-
-}
 </style>
