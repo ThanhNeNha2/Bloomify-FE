68d9db7956bdc2600832e0a8672f620a822e9eb8
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2026-04-19 01:37:01 +0700

    Refactor registration page to include Vue component setup and enhance greeting display. Update styles for improved layout and consistency.

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -1,13 +1,53 @@
+<script>
+import {
+  defineComponent,
+  ref
+} from 'vue';
+import RegisterPageContext from './RegisterPageContext';
+
+export default defineComponent({
+  name: 'RegisterView',
+
+  setup () {
+    const displayNameRef = ref('Vo Chi Thanh');
+
+    const context = RegisterPageContext.create({
+      displayNameRef
+    })
+      .setupComponent();
+
+    return {
+      context
+    };
+  }
+});
+</script>
+
 <template>
   <div class="unit-register">
-    <h1>Register</h1>
+    <p class="greeting">
+      {{ context.formatGreetingLine() }}
+    </p>
+
+    <p class="name">
+      Vo Chi Thanhaa
+    </p>
   </div>
 </template>
 
-<script setup>
+<style scoped>
+.unit-register {
+  background-color: var(--background-color);
 
-</script>
+  padding-block: 1rem;
+  padding-inline: 1rem;
+}
 
-<style lang="scss" scoped>
+.unit-register > .greeting {
+  margin-block-end: 0.5rem;
+}
 
+.unit-register > .name {
+  color: var(--color-brown);
+}
 </style>
